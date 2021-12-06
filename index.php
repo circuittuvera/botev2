cabd615aeb088145ad0a0bfc0f61d299d97138ce
circuittuvera <huffkinson@gmail.com>
2021-12-06 10:37:07 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BOACTS | Welcome</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- new datatable link and script -->
+	<link rel="stylesheet" type="text/css" href="datatables.min.css"/>
+ 	<script type="text/javascript" src="datatables.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+	<script src="jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+
+</head>
+<body>
+<!-- header starts here -->
+<header>
+<!-- navigation starts here -->
+<nav class="navbar navbar-dark ml-auto bg-dark sticky-top">
+	<a class="navbar-brand" href="#"><i class="fa fa-home"></i> BOACTS</a>
+	<!-- hamburger menu starts here -->
+	<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<!-- hamburger menu ends here -->
+
+	<!-- dropdown menu starts here -->
+	<!-- <div class="dropdown d-flex justify-content-end">
+		<button class="mr-auto p2 btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Dropdown button
+  		</button>
+  		<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    		<a class="dropdown-item" href="#">Action</a>
+    		<a class="dropdown-item" href="#">Another action</a>
+    		<a class="dropdown-item" href="#">Something else here</a>
+  		</div>
+	</div> -->
+	<!-- dropdown menu ends here -->
+
+	<!-- collapse menu starts here -->
+	<div class="collapse navbar-collapse" id="navMenu">
+		<!-- collapse menu items starts here -->
+		<ul class="navbar-nav align-items-center">
+      		<li class="nav-item active">
+        		<a class="nav-link" href="#home">Home</a>
+      		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="#aboutus">About Us</a>
+      		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="#contactus">Contact Us</a>
+      		</li>
+      		<li>
+      			<a class="nav-link" href="login.php">Login</a>
+      		</li>
+    	</ul>
+    	<!-- collapse menu items ends here -->
+	</div>
+	<!-- collapse menu ends here -->
+</nav>
+<!-- navigtion ends here -->
+</header>
+<!-- header ends here -->
+
+<a name="home"></a>
+<!-- main content starts here -->
+<div class="container-fluid">
+	<!-- main content ends here -->
+	<div class="jumbotron" style="margin-top:1em;">
+		<div class="row">
+			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+				<img class="img-thumbnail" src="img/bote_logo.jpg" style="">
+			</div>
+			<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+				<h1>Bote Accounting and Taxation Services</h1>
+				<p style="font-size: 24px;">Providing our clients with excellent, innovative, and enterprising solutions</p>
+			</div>
+		</div>
+	</div>
+	<!-- main content ends here -->
+</div>
+
+<a name="aboutus"></a>
+<!-- about us starts here -->
+<div class="container-fluid">
+	<div class="jumbotron" style="margin-top:1em;">
+		<h1>Our commitment is to provide our clients with excellent, innovative and enterprising solutions and services.</h1>
+		<p class="text-dark" style="font-size: 24px;"><i class="text-success fa fa-check"></i> Board of Accountancy (BOA) accredited</p>
+		<p class="text-dark" style="font-size: 24px;"><i class="text-success fa fa-check"></i> Bureau of Internat Revenue (BIR) accredited</p>
+		<p class="text-dark" style="font-size: 24px;"><i class="text-success fa fa-check"></i> Certified Public Accountants (CPA)</p>
+		<p class="text-dark" style="font-size: 24px;"><i class="text-success fa fa-check"></i> Certified Internal Auditors</p>
+		<a class="btn btn-outline-primary btn-sm" href="#home" role="button">Back to top</a>
+	</div>
+	
+</div>
+<!-- about us ends here -->
+<a name="contactus"></a>
+<!-- contact us starts here -->
+<div class="container-fluid">
+	<div class="jumbotron" style="margin-top:1em;">
+		<div class="row justify-content-center">
+			<div class="col">
+				<h1>Get in touch with us!</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="card col-xs-12 col-sm-12 col-md-4 col-lg-4">
+				<div class="card-header">
+					<i class="fa fa-home"></i> <b>Office Address</b>
+				</div>
+				<div class="card-body">
+				<p>
+					1052 Dela Cruz Compound Interior,Quirino Highway
+				</p>
+				<p>
+					Brgy. Sta. Monica Novaliches, Quezon City
+				</p>
+				</div>
+			</div>
+			<div class="card col-xs-12 col-sm-12 col-md-4 col-lg-4">
+				<div class="card-header">
+					<i class="fa fa-phone"></i> <b>Contact Info</b>
+				</div>
+				<div class="card-body">
+				<p>
+					Phone: 417-4292 / 417-6328 / 794-5332
+				</p>
+				<p>
+					Email: boteacctgandtaxationservices@gmail.com
+				</p>
+				</div>
+			</div>
+			<div class="card col-xs-12 col-sm-12 col-md-4 col-lg-4">
+				<div class="card-header">
+					<i class="fa fa-clock"></i> <b>Office Hours</b>
+				</div>
+				<div class="card-body">
+				<p>
+					Monday to Friday
+				</p>
+				<p>
+					8AM to 5PM (PST)
+				</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col mt-2">
+				<a class="btn btn-outline-primary btn-sm" href="#home" role="button">Back to top</a>
+			</div>
+		</div>
+	</div>
+
+</div>
+<!-- contact us ends here -->
+<footer class="container-fluid text-center">
+	
+</footer>
+
+</body>
+</html>
